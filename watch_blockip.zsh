cba5c692ea03367659e7408223c316107e219f1a
zhouxuyujp <26949239+zhouxuyujp@users.noreply.github.com>
2024-09-13 03:25:08 +0900

    Update watch_blockip.zsh

--- watch_blockip.zsh
+++ watch_blockip.zsh
@@ -1,6 +1,5 @@
 #!/bin/zsh
 
-# 监听 blockip.txt 文件的更改（包括修改和关闭写操作）
 while inotifywait -e modify,close_write "$HOME/blockip.txt"; do
   echo "blockip.txt modified: Applying new IP block rules"
   "$HOME/update_iptables.sh"
